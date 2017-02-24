6f5733f35d9f5da6aac0dba43c8fffc2f14167e0
mahesh.giri <mahesh.giri6486@gmail.com>
2017-02-25 01:31:02 +0530

    awesome thing created

---
+++ mobile/src/main/java/android/hunger/expensetracker/MainExpenseRecyAdaptor.java
@@ -0,0 +1,29 @@
+package android.hunger.expensetracker;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by hunger on 2/25/2017.
+ */
+
+
+public class MainExpenseRecyAdaptor extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
